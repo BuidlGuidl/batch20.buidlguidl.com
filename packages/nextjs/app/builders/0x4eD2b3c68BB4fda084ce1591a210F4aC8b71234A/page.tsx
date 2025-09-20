d0890f920bcf7c519027e954b59686701e30ab18
timkot <timofey.kotov@yandex.ru>
2025-09-20 10:22:10 +0100

    fix: update Wagmi setup

---
+++ packages/nextjs/app/builders/0x4eD2b3c68BB4fda084ce1591a210F4aC8b71234A/page.tsx
@@ -0,0 +1,10 @@
+import ClientPage from "./ClientPage";
+
+export const metadata = {
+  title: "Timkot – Batch #20 Builder",
+  description: "Timkot’s personal page on Batch20",
+};
+
+export default function Page() {
+  return <ClientPage />;
+}
