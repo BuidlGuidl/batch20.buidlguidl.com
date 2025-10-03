2abbc20b6b511a38c4de72427e86c915525bc9c4
melanke <melanke.hc@gmail.com>
2025-10-03 17:12:01 -0300

    fix header -.-

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -90,8 +90,8 @@ export const Header = () => {
             <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.png" />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-ETH</span>
-            <span className="text-xs">Ethereum dev stack</span>
+            <span className="font-bold leading-tight">Batch #20</span>
+            <span className="text-xs">Graduation projects — builders showcase</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
